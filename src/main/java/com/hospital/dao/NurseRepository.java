0db245e66990774ab44195dd78afd5575f134643
scort <scort@LAPTOP-J3K488UP>
2024-11-07 16:56:52 +0100

    Improve logic to no get all nurses and then check user and password #31

--- src/main/java/com/hospital/dao/NurseRepository.java
+++ src/main/java/com/hospital/dao/NurseRepository.java
@@ -2,6 +2,7 @@ package com.hospital.dao;
 
 import org.springframework.data.repository.CrudRepository;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -11,4 +12,7 @@ public interface NurseRepository extends CrudRepository<Nurse, Integer> {
 	@Query("SELECT n FROM Nurse n WHERE n.name LIKE %:name%")
 	List<Nurse> findByNameContaining(@Param("name") String name);
 
+	// Método para buscar por username y password
+	@Query("SELECT n FROM Nurse n WHERE n.username = :username AND n.password = :password")
+	Optional<Nurse> findByUsernameAndPassword(@Param("username") String username, @Param("password") String password);
 }
