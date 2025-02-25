d573a4a556803525d623a2a80f68acb69103147e
Jordi Vidal <163132865+reivaX1755@users.noreply.github.com>
2025-02-25 17:09:49 +0100

    getallnurses endpoint created, with roomwrapper to return propperly allrooms, and roomrepository ro use .findAll() on rooms.

---
+++ src/main/java/entity/Room.java
@@ -0,0 +1,47 @@
+package entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "room")
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @JsonProperty("room_number")
+    private Integer roomNumber;
+
+    @JsonProperty("floor")
+    private Integer floor;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Integer getFloor() {
+        return floor;
+    }
+
+    public void setFloor(Integer floor) {
+        this.floor = floor;
+    }
+}
