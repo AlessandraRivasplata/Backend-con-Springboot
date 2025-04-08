6a21958c95f68212cdbd44ed7b067fa0356916e6
Jordi Vidal <163132865+reivaX1755@users.noreply.github.com>
2025-04-08 18:50:58 +0200

    Deleted drainage class and updated care class to use the existing endpoint

--- src/main/java/com/hospital/dao/DietRepository.java
+++ src/main/java/com/hospital/dao/DietRepository.java
@@ -1,9 +1,1 @@
 package com.hospital.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import entity.Drainage;
-
-public interface DietRepository extends CrudRepository<Drainage, Integer>{
-
-}
